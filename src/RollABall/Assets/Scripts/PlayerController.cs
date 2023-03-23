8dce5dfc202b035d41870b1694240d3cd700e4d6
TetieWasTaken <tjvssr@gmail.com>
2023-03-23 15:41:27 +0100

    feat: add player movement

---
+++ src/RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+
+    private Rigidbody rb;
+    private float moveX;
+    private float moveY;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movementVector = new Vector3(moveX, 0.0f, moveY);
+
+        rb.AddForce(movementVector * speed);
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        
+        moveX = movementVector.x;
+        moveY = movementVector.y;
+    }
+}
