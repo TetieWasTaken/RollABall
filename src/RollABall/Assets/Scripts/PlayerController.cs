84ff0224318d8a8ae2395e390c941db840655e43
TetieWasTaken <tjvssr@gmail.com>
2023-03-23 16:24:32 +0100

    feat: add scoring system

--- src/RollABall/Assets/Scripts/PlayerController.cs
+++ src/RollABall/Assets/Scripts/PlayerController.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public float speed = 10;
+    public TextMeshProUGUI scoreText;
+    public GameObject winTextObject;
 
     private Rigidbody rb;
+    private int score = 0;
     private float moveX;
     private float moveY;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        SetCountText();
+        winTextObject.SetActive(false);
     }
 
     void FixedUpdate()
@@ -29,11 +36,21 @@ public class PlayerController : MonoBehaviour
         moveY = movementVector.y;
     }
 
+    void SetCountText()
+    {
+        scoreText.text = $"Score: {score}";
+        if (score >= 12)
+            winTextObject.SetActive(true);
+    }
+
     void OnTriggerEnter(Collider collided)
     {
        if (collided.gameObject.CompareTag("Pickup"))
        {
-           collided.gameObject.SetActive(false);
+            collided.gameObject.SetActive(false);
+            score++;
+
+            SetCountText();
        }
     }
 }
